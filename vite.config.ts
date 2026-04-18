4ef110ef52be8a3cfbcf312ec6324f692d2fe40f
Rahi09Suthar <rahisuthar7@gmail.com>
2026-04-18 16:06:55 +0530

    commit-2

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  
-  const env = loadEnv(mode, '.', '');
-  
-  return {
-    plugins: [react(), tailwindcss()],
-    
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
-    
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
 
-    server: {
-      // for runnig the site in link.
-      allowedHosts: ['webnova-ako7.onrender.com'],
-
-      // existing config
-      hmr: process.env.DISABLE_HMR !== 'true',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-  };
+  },
+
+  server: {
+    allowedHosts: ['webnova-ako7.onrender.com'],
+  },
 });
